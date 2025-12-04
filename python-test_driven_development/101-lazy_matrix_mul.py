f2bb78f7c73b1bda4cec138ba66aa96683ee942e
“had271” <Hadeelmesnad@gmail.comgit config --global user.name had271git config --global user.email Hadeelmesnad@gmail.com>
2025-12-04 22:00:35 +0000

    edit file

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -2,34 +2,13 @@
 """Defines a matrix multiplication function using NumPy."""
 import numpy as np
 
+
 def lazy_matrix_mul(m_a, m_b):
-    # Validate m_a
-    if not isinstance(m_a, list) or m_a == [] or m_a == [[]]:
-        raise TypeError("Scalar operands are not allowed, use '*' instead")
+    """Return the multiplication of two matrices.
 
-    if not isinstance(m_b, list) or m_b == [] or m_b == [[]]:
-        raise TypeError("Scalar operands are not allowed, use '*' instead")
+    Args:
+        m_a (list of lists of ints/floats): The first matrix.
+        m_b (list of lists of ints/floats): The second matrix.
+    """
 
-    # Validate elements inside
-    for row in m_a:
-        if not isinstance(row, list):
-            raise TypeError("Scalar operands are not allowed, use '*' instead")
-        for x in row:
-            if not isinstance(x, (int, float)):
-                raise TypeError("Scalar operands are not allowed, use '*' instead")
-
-    for row in m_b:
-        if not isinstance(row, list):
-            raise TypeError("Scalar operands are not allowed, use '*' instead")
-        for x in row:
-            if not isinstance(x, (int, float)):
-                raise TypeError("Scalar operands are not allowed, use '*' instead")
-
-    # Check sizes
-    try:
-        res = np.matmul(np.array(m_a), np.array(m_b))
-    except ValueError:
-        raise TypeError("Scalar operands are not allowed, use '*' instead")
-
-    return res.tolist()
-
+    return (np.matmul(m_a, m_b))
