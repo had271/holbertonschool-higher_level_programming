75606bd72f959f9a399087711a4792c1028a81f3
“had271” <Hadeelmesnad@gmail.comgit config --global user.name had271git config --global user.email Hadeelmesnad@gmail.com>
2025-12-04 23:01:12 +0000

    Update

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -4,42 +4,36 @@ Multiplies two matrices
 """
 import numpy as np
 
+
 def lazy_matrix_mul(m_a, m_b):
     """Multiplies two matrices using NumPy."""
-    if not isinstance(m_a, list):
+
+    # scalar check
+    if not isinstance(m_a, list) or not isinstance(m_b, list):
         raise TypeError("Scalar operands are not allowed, use '*' instead")
-    if not isinstance(m_b, list):
-        raise TypeError("Scalar operands are not allowed, use '*' instead")
-    rows_a = len(m_a)
-    cols_a = len(m_a[0])
-    rows_b = len(m_b)
-    cols_b = len(m_b[0])
 
-    if cols_a != rows_b:
-        raise ValueError(
-            f"shapes ({rows_a},{cols_a}) and ({rows_b},{cols_b}) not aligned: "
-            f"{cols_a} (dim 1) != {rows_b} (dim 0)"
-        )
-
-    if cols_b != rows_a:
-        raise ValueError(
-            f"shapes ({rows_b},{cols_b}) and ({rows_a},{cols_a}) not aligned: "
-            f"{cols_b} (dim 1) != {rows_a} (dim 0)"
-        )
-    for row in m_a:
-    for ele in row:
-        if not isinstance(ele, (int, float)):
-            raise TypeError("invalid data type for einsum")
-
-    for row in m_b:
-    for ele in row:
-        if not isinstance(ele, (int, float)):
-            raise TypeError("invalid data type for einsum")
     try:
         a = np.array(m_a)
         b = np.array(m_b)
+
+        # attempt multiplication
         return np.matmul(a, b)
+
     except ValueError as err:
-        raise ValueError(err)
+        # dimension mismatch: shapes info موجود داخل err
+        msg = str(err)
+        if "shapes" in msg:
+            raise ValueError(msg)
+        if "setting an array element with a sequence" in msg:
+            raise ValueError("setting an array element with a sequence.")
+        else:
+            raise ValueError(msg)
+
     except TypeError as err:
-        raise TypeError(err)
+        # wrong type inside matrix
+        msg = str(err)
+        if "data type" in msg or "numpy" in msg:
+            raise TypeError("invalid data type for einsum")
+        else:
+            raise TypeError(msg)
+
