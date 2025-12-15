d119e88ca332a47fd68fae690f3468ad5a9bb2b7
“had271” <Hadeelmesnad@gmail.comgit config --global user.name had271git config --global user.email Hadeelmesnad@gmail.com>
2025-12-15 06:48:01 +0000

    edit

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -7,4 +7,7 @@ class MyList(list):
 
     def print_sorted(self):
         """Print a list in sorted ascending order."""
+        for i in self:
+            if not isinstance(i, int):
+                raise TypeError("unorderable types: str() < int()")
         print(sorted(self))
