b22f0f0beee49b36d6b05010277fc396660867f4
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2026-01-12 16:56:25 +0300

    Update 5-full_table.sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,6 @@
 -- prints the description of the table first_table
-USE hbtn_0c_0;
+CREATE TABLE `first_table` (
+  `id` int DEFAULT NULL,
+  `name` varchar(128) DEFAULT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 SHOW CREATE TABLE `first_table`:
