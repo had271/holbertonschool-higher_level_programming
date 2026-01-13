3f99f02157702a00c559de5e6e85d2d98d249f59
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2026-01-13 15:30:06 +0300

    Update 2-create_read_user.sql

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,7 @@
 -- Create database hbtn_0d_2
 CREATE DATABASE hbtn_0d_2;
+-- Drop if the user user_0d_1 already exists
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
 -- Create user_0d_2
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 --  SELECT privilege for user_0d_2
