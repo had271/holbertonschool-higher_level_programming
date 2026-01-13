5afb77483a6e71ac57da500aa5ed8fef0035985c
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2026-01-13 15:13:29 +0300

    Update 0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
--- Lists all privileges of the MySQL users 
-SELECT User FROM mysql.user;
+-- Lists all privileges of the MySQL users  user_0d_1 and user_0d_2
+SHOW GRANT FOR 'user_0d_1'@'localhost'
+SHOW GRANT FOR 'user_0d_2'@'localhost'
