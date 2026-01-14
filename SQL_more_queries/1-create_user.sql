c99cdc02bce4196d9c01edbfd92be37f9d245c34
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2026-01-14 16:06:39 +0300

    Update 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -3,4 +3,4 @@ DROP USER IF EXISTS 'user_0d_1'@'localhost';
 -- Creates the MySQL server user user_0d_1.
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- Set to have all privileges on your MySQL server
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd' FLUSH PRIVILEGES;
