52af038e6f0adb5575599a98cb5ae9788a55fdc4
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2026-01-14 18:18:20 +0300

    Update 15-comedy_only.sql

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -2,6 +2,6 @@
 SELECT s.title
 FROM tv_shows AS s
   INNER JOIN tv_show_genres AS t ON t.show_id = s.id
-  INNER JOIN tv_genres AS g ON g.id = t.gener_id 
+  INNER JOIN tv_genres AS g ON g.id = t.genre_id 
 WHERE g.name = 'Comedy'
 ORDER BY s.title ASC;
