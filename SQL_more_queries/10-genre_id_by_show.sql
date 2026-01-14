68dfd5b45f200ed1959b004a5654ada2062602f4
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2026-01-14 16:43:52 +0300

    Update 10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,6 @@
 -- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-SELECT g.genre_id, t.title
-FROM tv_shows AS t
-INNER JOIN tv_show_genres AS g ON g.genre_id = t.show_id
-ORDER BY t.title, g.genre_id;
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+        INNER JOIN `tv_show_genres` AS g
+	ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
