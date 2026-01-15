3cbac22d29d6b0089dff61d72af56b3cf9072bed
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2026-01-15 16:18:24 +0300

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
-'''Lists all states from a MySQL database.'''
-import sys
-import MySQLdb
+"""Script displays states in database by ascending id order
+   Script should take 3 arguments:
+   mysql username, mysql password and database name
+"""
 
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
 
-if __name__ == "__main__":
-    user = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=user,
-        passwd=password, 
-        db=db_name)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
-    for state in cur.fetchall():
-        print(state)
+    if len(sys.argv) > 3:
+        conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            charset="utf8"
+        )
+        cur = conn.cursor()
+        cur.execute("""SELECT * FROM states ORDER BY states.id ASC""")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        conn.close()
