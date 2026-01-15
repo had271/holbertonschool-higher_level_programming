ed2fa15e6485f1719061adee1bd8714dbf76e118
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2026-01-15 15:29:58 +0300

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -14,3 +14,5 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     [print(state) for state in cur.fetchall()]
+    cur.close()
+    db.close()
