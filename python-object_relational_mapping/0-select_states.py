bf640ae7d749a99ed4ab545b5f0919e7038cf13a
“had271” <Hadeelmesnad@gmail.comgit config --global user.name had271git config --global user.email Hadeelmesnad@gmail.com>
2026-01-15 11:30:02 +0000

    add file 0-select_states.py

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Lists all states from a MySQL database."""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    for state in cur.fetchall():
+        print(state)
+    cur.close()
+    db.close()
